280e6dac7d19eda3da4464c2ac797ce78d675191
Francisco Tavares <francisco.tln@outlook.com>
2018-03-26 00:07:01 +0200

    prevent producer before start === 0. Make sure producer is function. prevent producer from sinking (0, any)

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const create = prod => (start, sink) => {
+  if (start !== 0) return;
   let got2 = false;
   let disposed = false;
   function checkGot2(t) {
     got2 = got2 || t === 2;
   }
-  start === 0 && sink(start, checkGot2);
-  prod && prod((t, d) => {
+  sink(start, checkGot2);
+  if (typeof prod !== 'function') return;
+  prod((t, d) => {
+    if (t === 0) return;
     checkGot2(t);
     !got2 && sink(t, d);
     if (got2 && !disposed) {
