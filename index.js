473f7f74b646d7bc398d64dfc726bbf197882c71
Francisco Tavares De Lima Neto <francisco.tln@outlook.com>
2019-07-23 14:49:03 +0200

    2.1.0

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const create = prod => (start, sink) => {
-	if (start !== 0) return;
-	if (typeof prod !== 'function') {
-		sink(0, () => {});
-		sink(2);
-		return;
-	}
-	let end = false;
-	let clean;
-	sink(0, (t) => {
-		if (!end) {
-			end = t === 2;
-			if (end && typeof clean === 'function') clean();
-		}
-	});
-	if (end) return;
-	clean = prod((v) => {
-		if (!end) sink(1, v);
-	}, (e) => {
-		if (!end && e !== undefined) {
-			end = true;
-			sink(2, e);
-		}
-	}, () => {
-		if (!end) {
-			end = true;
-			sink(2);
-		}
-	});
+  if (start !== 0) return;
+  if (typeof prod !== 'function') {
+    sink(0, () => {});
+    sink(2);
+    return;
+  }
+  let end = false;
+  let clean;
+  sink(0, (t) => {
+    if (!end) {
+      end = t === 2;
+      if (end && typeof clean === 'function') clean();
+    }
+  });
+  if (end) return;
+  clean = prod((v) => {
+    if (!end) sink(1, v);
+  }, (e) => {
+    if (!end && e !== undefined) {
+      end = true;
+      sink(2, e);
+    }
+  }, () => {
+    if (!end) {
+      end = true;
+      sink(2);
+    }
+  });
 };
 
 export default create;
